e15530d8043b0f6c005d6ab1f9f7a080c9ffa514
massag03 <gregorio.massara1@bbc.co.uk>
2024-08-16 16:47:03 +0100

    Add btn functionality to show a list (recyclable view) and store state to handle visibility

--- app/src/main/java/uk/co/bbc/orjetpack/MainActivity.kt
+++ app/src/main/java/uk/co/bbc/orjetpack/MainActivity.kt
@@ -1,12 +1,12 @@
 package uk.co.bbc.orjetpack
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,6 +28,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,8 +46,7 @@ class MainActivity : ComponentActivity() {
             ORJetPackTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     CreateBizCard()
                 }
@@ -54,6 +57,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -82,23 +88,22 @@ fun CreateBizCard() {
                     thickness = 1.dp
                 )
                 CreateInfo(
-                    name = "Gregorio",
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
+                    name = "Gregorio", modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
-                Button(modifier = Modifier.padding(1.dp),
-                    onClick = {
-                        Log.d("DEBUG", "Button Clicked!")
-
-                    }
-                ) {
+                Button(modifier = Modifier.padding(1.dp), onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
                     Text(
                         text = "Portfolio",
                         modifier = Modifier.padding(2.dp),
                         style = MaterialTheme.typography.titleLarge
                     )
-
                 }
+            }
+            if (buttonClickedState.value) {
+                Content()
+            } else {
+                Box {}
             }
         }
     }
@@ -124,6 +129,41 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
     }
 }
 
+
+@Preview
+@Composable
+private fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.Red)
+        ) {
+            Portfolio(data = listOf("Project1", "Project2", "Project3"))
+
+        }
+    }
+
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) {
+            Text(text = it)
+        }
+
+    }
+}
+
 @Composable
 private fun CreateInfo(name: String, modifier: Modifier = Modifier) {
     Column(
@@ -139,8 +179,7 @@ private fun CreateInfo(name: String, modifier: Modifier = Modifier) {
             style = MaterialTheme.typography.headlineMedium,
         )
         Text(
-            text = "Android Composer",
-            modifier = Modifier.padding(5.dp)
+            text = "Android Composer", modifier = Modifier.padding(5.dp)
         )
         Text(
             text = "@gmassara",
