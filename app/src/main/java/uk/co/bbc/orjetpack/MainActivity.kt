bf6435b198b91aee241b0d320b0ceda1220a4bae
massag03 <gregorio.massara1@bbc.co.uk>
2024-08-16 12:27:36 +0100

    Add profile picture, Divider and Text.

--- app/src/main/java/uk/co/bbc/orjetpack/MainActivity.kt
+++ app/src/main/java/uk/co/bbc/orjetpack/MainActivity.kt
@@ -3,24 +3,32 @@ package uk.co.bbc.orjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.co.bbc.orjetpack.ui.theme.ORJetPackTheme
@@ -44,30 +52,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(12.dp),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(24.dp),
+            shape = RoundedCornerShape(
+                corner = CornerSize(20.dp)
+            ),
             colors = CardDefaults.cardColors(Color.White),
-            elevation = CardDefaults.cardElevation(4.dp)
+            elevation = CardDefaults.cardElevation(8.dp)
         ) {
-
+            Column(
+                modifier = Modifier.height(400.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile(modifier = Modifier.padding(40.dp))
+                Divider(
+                    modifier = Modifier.padding(20.dp, top = 0.dp, bottom = 20.dp), color = Color.Red,
+                    thickness = 1.dp
+                )
+                Greeting(
+                    name = "Gregorio",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
         }
-
     }
 
+}
+
+// we can pass an optional Modifier into our function
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier,
+        shape = CircleShape,
+        border = BorderStroke(1.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.me_fixed),
+            contentDescription = "Gregorio image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier,
+        color = Color.Black
     )
 }
 
